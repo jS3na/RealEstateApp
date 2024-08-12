974d2964618a795d1c9cc23f5dbb54de47116ad1
João Sena <126720426+jS3na@users.noreply.github.com>
2024-08-12 12:53:16 -0300

    turn

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,32 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function TabLayout() {
+    return (
+        <Tabs screenOptions={{ headerShown:false }} >
+            <Tabs.Screen 
+                name="Home"
+                options={{
+                    tabBarIcon: ({color}) => <AntDesign name="home" size={24} color="black" />
+                }}    
+            />
+            
+            <Tabs.Screen 
+                name="Explore"
+                options={{
+                    tabBarIcon: ({color}) => <AntDesign name="search1" size={24} color="black" />
+                }}    
+            />
+
+            <Tabs.Screen 
+            name="Profile"
+            options={{
+                tabBarIcon: ({color}) => <Ionicons name="person-outline" size={24} color="black" />
+            }}    
+        />
+        </Tabs>
+    )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,8 @@
-import { Text, View } from "react-native";
+import { Redirect } from 'expo-router';
+import React from 'react';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  
+  return <Redirect href={'/Home'} />
+
 }
