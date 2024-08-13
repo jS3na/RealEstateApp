e04a8374c0f343946f4321dd51a53fb1cbc2a47a
João Sena <126720426+jS3na@users.noreply.github.com>
2024-08-13 11:57:21 -0300

    installing firebase

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import { SignOutButton, useAuth } from '@clerk/clerk-expo';
+
 export default function Profile() {
+
+    const { user } = useAuth();
     return (
         <View>
             <Text>Profile</Text>
+            <SignOutButton signOutOptions={{ user }} />
         </View>
     )
 }
